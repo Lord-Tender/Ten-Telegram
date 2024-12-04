af83c00e50567263e8303aecc0718ae309b88aad
Emmanuel <emmanuelola961@gmail.com>
2024-12-04 05:43:08 +0100

    Work in progress...

--- Bots/launch.bot.js
+++ Bots/launch.bot.js
@@ -1,4 +1,4 @@
-const { Telegraf, Markup  } = require('telegraf')
+const { Telegraf, Markup } = require('telegraf')
 require('dotenv').config()
 const telegramToken = process.env.TELEGRAM_TOKEN
 const { saveUserInfo } = require('../Controllers/user.controller')
@@ -7,36 +7,47 @@ const { welcomeMsg } = require('./message.bot')
 const launchBot = () => {
     const bot = new Telegraf(telegramToken)
 
-    bot.start((ctx)=>{
+    bot.start((ctx) => {
         if (ctx.from.is_bot) {
             ctx.reply('Bot are not allowed');
             return;
         }
+        console.log("payload " + ctx.payload)
         saveUserInfo(ctx.from)
-        .then((data)=>{
-            console.log(data)
-            const msg = welcomeMsg()
-            ctx.reply(msg, 
-                Markup.keyboard([
-                    ['Daily Reward 🎁', 'Balance 💰'],
-                    ['Task 💼', 'Invite ✉'],
-                    ['Withdrawal 💳', 'Advertise 📢'],
-                    ['Join Us on all Social Media ✅'],
-                  ])
-                    .resize()
-                    .persistent()
-            )
-            
-        })
-        .catch((error)=>{
-            if (error.code == 11000) {
-                ctx.reply("You're already a user, Enjoy earning with us😉")
-                return;
-            }
+            .then((data) => {
+                const msg = welcomeMsg()
+                ctx.reply(`👋Hello, Welcome to Epidomax. https://t.me/tender_test_1bot?start=${data.data.userId}`,
+                    Markup.keyboard([
+                        ['Daily Reward 🎁', 'Balance 💰'],
+                        ['Task 💼', 'Invite ✉'],
+                        ['Withdrawal 💳', 'Advertise 📢'],
+                        ['Join Us on all Social Media ✅'],
+                    ])
+                        .resize()
+                        .persistent()
 
-            ctx.reply(error.errorMsg)
-        })
+                )
+                ctx.reply(msg,
+                    Markup.inlineKeyboard([
+                        Markup.button.callback('Check', 'CHECK'),
+                    ])
+                )
+
+            })
+            .catch((error) => {
+                if (error.code == 11000) {
+                    ctx.reply("You're already a user, Enjoy earning with us😉")
+                    return;
+                }
+
+                ctx.reply(error.errorMsg)
+            })
     })
+
+    bot.action('CHECK', (ctx) => {
+        ctx.answerCbQuery();
+        ctx.reply('Are you sure');
+    });
 
     bot.launch()
     console.log('Bot launched!!!')
--- Bots/message.bot.js
+++ Bots/message.bot.js
@@ -1,5 +1,5 @@
 const welcomeMsg = () => {
-  return `👋Hello, Welcome to Earner bot \nEarner bot is a platform where you can make money by completing simple Task. \n\n 🎁 ₦1,000 Daily reward \n 🎁 ₦500 minimum task reward \n 🎁 ₦2,000 Referral bonus \n\nTo start earning with us, Join and Subcribe to our communities: \n\n◀ Join👉 @tenderseries`;
+  return `Epidomax is a platform where you can make money by completing simple Task. \n\n 🎁 ₦1,000 Daily reward \n 🎁 ₦500 Minimum task reward \n 🎁 ₦2,000 Referral bonus \n 🎁 ₦2,500 Welcoming bonus. \n\nPlease complete the task below to receive your welcoming bonus, Join and Subcribe to our communities: \n\n◀ Join👉 @tenderseries \n\nWhen you're done, click on "Check" below.`;
 }
 
 module.exports = {
